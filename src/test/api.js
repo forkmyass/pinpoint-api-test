858fa5cd5739b8b9bc16b78c59b76b66b79f4988
Sergey <sva@ciklum.com>
2015-06-24 14:54:56 +0300

    remove only

--- src/test/api.js
+++ src/test/api.js
@@ -181,7 +181,7 @@ describe("AdminAdvertiser", () => {
             }
         });
 
-        it.only("should return advertiser info after creation advertiser by admin", async (done) => {
+        its("should return advertiser info after creation advertiser by admin", async (done) => {
             try {
                 let advertiser = await createAdvertiser(Object.assign(data.advertiser, {Email: "test@email.com" + Date.now()}));
 
